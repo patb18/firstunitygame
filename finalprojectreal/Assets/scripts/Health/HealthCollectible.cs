2ed1cf9a139b0c22d4b19451b3feaf9917301652
patb18 <patrickbotros18@gmail.com>
2023-06-14 18:16:24 -0400

    iframes
    added iframes

--- finalprojectreal/Assets/scripts/Health/HealthCollectible.cs
+++ finalprojectreal/Assets/scripts/Health/HealthCollectible.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 public class HealthCollectible : MonoBehaviour
 {
     [SerializeField] private float healthValue;
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Player")
+        if (collision.tag == "Player")
         {
-            collision.GetComponent<HealthCollectible>().AddHealth(healthValue);
+            collision.GetComponent<Health>().AddHealth(healthValue);
             gameObject.SetActive(false);
         }
     }
